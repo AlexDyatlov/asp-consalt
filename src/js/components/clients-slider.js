09632ae43f74e85c55b8a382e87f758134479064
AlexDyatlov <alexdyatlov.web@gmail.com>
2021-03-14 01:20:37 +0300

    added adaptive index, price, contacts

--- src/js/components/clients-slider.js
+++ src/js/components/clients-slider.js
@@ -1,7 +1,21 @@
 const clientsSlider = new Swiper('.clients-slider', {
   loop: true,
-  slidesPerView: 4,
+  slidesPerView: 1,
   autoplay: {
     delay: 3000,
   },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    760: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1025: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
 });
